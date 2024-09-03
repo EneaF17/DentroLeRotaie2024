e8f675c5b7d63463da8d55058a88acb0ea4a6128
Enea Fascilla <enea.fascilla@gmail.com>
2024-09-03 12:35:04 +0200

    primo Commit base

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE php>
+<php lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DentroLeRotaie2024</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</php>
